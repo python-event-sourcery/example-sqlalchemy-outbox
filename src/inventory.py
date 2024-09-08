7b865e19dfaf513459d6ba77d7d946b44d385f3c
Łukasz Żukowski <lukasz.zukowski@gmail.com>
2024-09-08 14:33:49 +0200

    Inventory: Create item

--- src/inventory.py
+++ src/inventory.py
@@ -1,4 +1,43 @@
-from fastapi import APIRouter, Body
+from collections.abc import Generator
+from dataclasses import dataclass
+from typing import ClassVar
+
+from event_sourcery.aggregate import Aggregate, Repository
+from event_sourcery.event_store import (
+    Backend,
+    Event,
+    StreamUUID,
+)
+from fastapi import APIRouter, Body, Depends
+
+from backend import backend
+
+
+@dataclass
+class Quantity(Aggregate):
+    category: ClassVar[str] = "quantity"
+    quantity: int = 0
+
+    class Adjust(Event):
+        quantity: int
+
+    def adjust(self, by: int) -> None:
+        self._emit(self.Adjust(quantity=self.quantity + by))
+
+    def __apply__(self, event: Event) -> None:
+        if type(event) is Quantity.Adjust:
+            self.quantity = event.quantity
+
+
+class QuantityRepository(Repository[Quantity]):
+    pass
+
+
+def quantity_repository(
+    backend: Backend = Depends(backend),
+) -> Generator[QuantityRepository, None, None]:
+    yield QuantityRepository(backend.event_store)
+
 
 router = APIRouter(
     prefix="/inventory",
@@ -11,5 +50,10 @@ router = APIRouter(
     name="inventory:create_item",
     status_code=201,
 )
-def create_item(item: str = Body(...), quantity: int = Body(...)) -> None:
-    raise NotImplementedError
+def create_item(
+    item: str = Body(...),
+    quantity: int = Body(...),
+    repository: QuantityRepository = Depends(quantity_repository),
+) -> None:
+    with repository.aggregate(StreamUUID(name=item), Quantity()) as aggregate:
+        aggregate.adjust(by=quantity)
--- src/ordering.py
+++ src/ordering.py
@@ -1,6 +1,17 @@
-from fastapi import APIRouter
+from uuid import UUID
+
+from fastapi import APIRouter, Body
 
 router = APIRouter(
     prefix="/orders",
     tags=["orders"],
 )
+
+
+@router.post("/", name="orders:create_order", status_code=201)
+def create_order(
+    order_id: UUID = Body(...),
+    item_name: str = Body(...),
+    units: int = Body(...),
+) -> None:
+    raise NotImplementedError
