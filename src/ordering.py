5a203f72fa5910b01a00d98f5a209958704f2286
Łukasz Żukowski <lukasz.zukowski@gmail.com>
2024-09-08 14:35:40 +0200

    Ordering: Create order

--- src/ordering.py
+++ src/ordering.py
@@ -1,6 +1,9 @@
 from uuid import UUID
 
-from fastapi import APIRouter, Body
+from event_sourcery.event_store import Backend, Event, StreamId
+from fastapi import APIRouter, Body, Depends
+
+from backend import backend
 
 router = APIRouter(
     prefix="/orders",
@@ -8,10 +11,19 @@ router = APIRouter(
 )
 
 
+class OrderPlaced(Event):
+    item: str
+    units: int
+
+
 @router.post("/", name="orders:create_order", status_code=201)
 def create_order(
     order_id: UUID = Body(...),
     item_name: str = Body(...),
     units: int = Body(...),
+    backend: Backend = Depends(backend),
 ) -> None:
-    raise NotImplementedError
+    backend.event_store.append(
+        OrderPlaced(item=item_name, units=units),
+        stream_id=StreamId(order_id, category="orders"),
+    )
--- src/outbox.py
+++ src/outbox.py
@@ -4,3 +4,8 @@ router = APIRouter(
     prefix="/outbox",
     tags=["outbox"],
 )
+
+
+@router.post("/process", name="outbox:process", status_code=200)
+def process() -> None:
+    raise NotImplementedError
